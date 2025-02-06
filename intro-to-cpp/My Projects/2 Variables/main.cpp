d95c1494d2efe4cad26f44ef6188217ab10b63d1
Keppibara <Blakekemp2006@outlook.com>
2025-02-06 12:30:34 +1100

    2 variables

--- intro-to-cpp/My Projects/2 Variables/main.cpp
+++ intro-to-cpp/My Projects/2 Variables/main.cpp
@@ -5,41 +5,35 @@
 int main()
 {
 
-	// main.cpp/main()
-	// ...
-	//A) My guess was 9
-	// answer was 14
-
+	// A)
 
 	int numberA = 5;
 
-	numberA = 9;
-	numberA = 11;
-	numberA = 15;
-
+	{
+		numberA = 9;
+		numberA = 11;
+		numberA = 14;
+	}
 
 	std::cout << "A)" << numberA << std::endl;
 
-	//...
+	
 
-
+	// B)
 	{
-		//B)
-		// My guess is 22
-		// Answer was 9
+
 		int numberB = 10;
 		numberB = 9;
 
 		int thingB = 55;
-		thingB = 22,
+		thingB = 22;
 
-			std::cout << "B)" << numberB << std::endl;
+
+		std::cout << "B)" << numberB << std::endl;
 	}
 
 	{
-		//C)
-		// My guess is 1
-		// Answer was 21
+	
 		int numberC = 3;
 		numberC = 7;
 		numberC = 1;
@@ -53,9 +47,7 @@ int main()
 
 	}
 	{
-		//D
-		// My guess is 3
-		// Answer was 3
+		
 		int numberD = 1;
 		int somethingD = 3;
 
@@ -66,9 +58,7 @@ int main()
 	}
 
 	{
-		// E)
-		// My guess is 6.5
-		// Answer was 6
+		
 		int x = 13;
 
 		x = x / 2;
@@ -77,9 +67,7 @@ int main()
 	}
 
 	{
-		// F)
-		//My guess is 6
-		//Answer is 2 
+		
 		float y = 13;
 
 		y = y = 2;
