6d8a4f7a75f2e02be795a97ba4699e3d0524a46b
Keppibara <Blakekemp2006@outlook.com>
2025-02-13 16:53:07 +1100

    almost finished arrays in ZORP game

--- intro-to-cpp/My Projects/Zorp/Zorp.cpp
+++ intro-to-cpp/My Projects/Zorp/Zorp.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <Windows.h>
+#include <random>	
+#include <time.h>
 
 const char* CSI = "\x1b[";
 const char* TITLE = "\x1b[5;20h";
@@ -12,6 +14,18 @@ const char* RESTORE_CURSOR_POS = "\x1b[u";
 
 void main()
 {
+	const int EMPTY = 0;
+	const int ENEMY = 1;
+	const int TREASURE = 2;
+	const int FOOD = 3;
+	const int ENTRANCE = 4;
+	const int EXIT = 5;
+	const int MAX_RANDOM_TYPE = FOOD+1;
+
+	const int MAZE_WIDTH = 10;
+	const int MAZE_HEIGHT = 6;
+
+	//Set output mode to handle virtual terminal sequences
 	DWORD dwMode = 0;
 	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	GetConsoleMode(hOut, &dwMode);
@@ -22,12 +36,44 @@ void main()
 	char firstLetterOfName = 0;
 	int avatarHP = 0;
 
+	// create a 2d array
+	int rooms[MAZE_HEIGHT][MAZE_WIDTH];
+		srand(time(nullptr));
+
+		// fill the arrays with random room types
+		for (int y = 0; y < MAZE_HEIGHT; y++)
+		{
+			for (int x = 0; x < MAZE_WIDTH; x++) {
+				rooms[y][x] = rand() % MAX_RANDOM_TYPE;
+			}
+		}
+		// set the entrance and exit of the maze
+		rooms[0][0] = ENTRANCE;
+		rooms[MAZE_HEIGHT - 1][MAZE_WIDTH - 1] = EXIT;
+
 	std::cout << TITLE << MAGENTA << "Welcome to ZORP!" << RESET_COLOR << std::endl;
 	std::cout << INDENT << "ZORP is a game of adventure, danger, and low cunning." << std::endl;
 	std::cout << INDENT << "It is definitely not related to any other text-based adventure game." << std::endl << std::endl;
 
-	std::cout << "First, some questions..." << std::endl;
+	std::cout << INDENT << "First, some questions..." << std::endl;
+	
+	// save cursor position
 	std::cout << SAVE_CURSOR_POS;
+	
+	// output the map
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
+	for (int y = 0; y < MAZE_HEIGHT; y++)
+	{
+		std::cout << INDENT;
+		for (int x = 0; x < MAZE_WIDTH; x++) {
+			std::cout << "[ " << rooms[y][x] << " ] ";
+		}
+    	std::cout << std::endl;
+	}
+
 	std::cout << INDENT << "How tall are you, in centimeters? " << INDENT << YELLOW;
 
 	std::cin >> height;
