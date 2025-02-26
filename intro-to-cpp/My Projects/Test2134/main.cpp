e68cba2a5b725cb51ec55d752a624cb53f4788a2
Keppibara <Blakekemp2006@outlook.com>
2025-02-27 08:42:51 +1100

    random commit in my projects

---
+++ intro-to-cpp/My Projects/Test2134/main.cpp
@@ -0,0 +1,16 @@
+// Test2134.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <string>
+#include <iostream>
+
+using std::string;
+
+int main()
+{
+	char text = tolower('L');
+
+	std::cout << text;
+
+	return 0;
+}
+
